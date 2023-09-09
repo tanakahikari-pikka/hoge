eb760153a106cb720ea142b611373b0823278b3b
HiroMinase <lostsymbol612@gmail.com>
2023-09-09 15:52:36 +0900

    補足コメントを追加

--- lib/team_development_practice/todo_riverpod/sample/view.dart
+++ lib/team_development_practice/todo_riverpod/sample/view.dart
@@ -3,6 +3,8 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../todo.dart';
 
+// クラス名を自分なりに変えて実装を進めてください
+// 自分の名前を入れると他の人と被らないので良さそうです
 class TodosPage extends ConsumerWidget {
   const TodosPage({super.key});
 
