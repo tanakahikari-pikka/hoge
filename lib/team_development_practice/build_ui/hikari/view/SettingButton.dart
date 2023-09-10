6798f5227b23506678c09320c6c11b6dfb487ee8
Hikari-Tanaka-pikka <h.tanaka.22.7.575@4nonome.com>
2023-09-11 03:26:57 +0900

    ダイアログ

--- lib/team_development_practice/build_ui/hikari/view/SettingButton.dart
+++ lib/team_development_practice/build_ui/hikari/view/SettingButton.dart
@@ -1,44 +1,54 @@
 // ウィジェットの雛形を作成
 
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class SettingButton extends StatelessWidget {
   const SettingButton({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return IconButton(
-      icon: const Icon(Icons.settings),
-      onPressed: () {
-        showDialog(
-          context: context,
-          builder: (context) {
-            return AlertDialog(
-              content: const Text('システムダイアログです'),
-              actions: [
-                TextButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Colors.red,
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 5,
+        vertical: 10,
+      ),
+      child: IconButton(
+        icon: const Icon(Icons.settings),
+        onPressed: () {
+          showDialog<void>(
+            context: context,
+            builder: (context) => AlertDialog(
+              content: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const Text(
+                    "システムダイアログです",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  const Gap(16),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.red,
                     ),
-                    padding: const EdgeInsets.all(10),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
                     child: const Text(
-                      '閉じる',
+                      "閉じる",
                       style: TextStyle(
                         color: Colors.white,
                       ),
                     ),
                   ),
-                ),
-              ],
-            );
-          },
-        );
-      },
+                ],
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
